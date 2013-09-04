508bf3a93329ccff626289e5bc3466927a466b63
Ephraim <ephraim.pepe@gmail.com>
2013-09-04 12:00:41 +0200

    Count e translation in Controller/Traits/BaseController.php

--- Controller/Traits/BaseController.php
+++ Controller/Traits/BaseController.php
@@ -5,22 +5,30 @@ namespace Ephp\UtilityBundle\Controller\Traits;
 trait BaseController {
 
     /**
+     * 
+     * @return \Symfony\Bundle\FrameworkBundle\Translation\Translator
+     */
+    protected function getTranslator() {
+        return $this->get('translator');
+    }
+
+    /**
      * @return \Doctrine\ORM\EntityManager 
      */
     protected function getEm() {
         return $this->getDoctrine()->getManager();
     }
-    
+
     /**
      * Restituisce il repository richiesto
      * 
      * @param type $classe nome del repository
-     * @return @return \Doctrine\ORM\EntityRepository 
+     * @return \Doctrine\ORM\EntityRepository 
      */
     protected function getRepository($classe) {
         return $this->getEm()->getRepository($classe);
     }
-    
+
     /**
      * Fa il find da un repository
      * 
@@ -31,7 +39,7 @@ trait BaseController {
     protected function find($classe, $id) {
         return $this->getRepository($classe)->find($id);
     }
-    
+
     /**
      * Fa il findBy da un repository
      * 
@@ -42,7 +50,7 @@ trait BaseController {
     protected function findBy($classe, $find, $order = array(), $limit = null, $from = null) {
         return $this->getRepository($classe)->findBy($find, $order, $limit, $from);
     }
-    
+
     /**
      * Fa il findOneBy da un repository
      * 
@@ -53,7 +61,7 @@ trait BaseController {
     protected function findOneBy($classe, $find, $order = array()) {
         return $this->getRepository($classe)->findOneBy($find, $order);
     }
-    
+
     /**
      * Restituisce il repository richiesto
      * 
@@ -63,7 +71,7 @@ trait BaseController {
         $this->getEm()->persist($entity);
         $this->getEm()->flush();
     }
-    
+
     /**
      * Restituisce il repository richiesto
      * 
@@ -73,30 +81,76 @@ trait BaseController {
         $this->getEm()->remove($entity);
         $this->getEm()->flush();
     }
-    
+
+    /**
+     * 
+     * @param string $name
+     * @param mixed $default
+     * @return mixed
+     * @throws \Exception
+     */
     protected function getParam($name, $default = null) {
         $out = $this->getRequest()->get($name, null);
-        if($out === null) {
-            if($default instanceof \Exception) {
+        if ($out === null) {
+            if ($default instanceof \Exception) {
                 throw $default;
             }
             $out = $default;
         }
         return $out;
     }
-    
+
     protected function hasRole($role) {
         $user = $this->getUser();
         return $user->hasRole($role);
     }
-    
+
     protected function inRole($roles) {
         $user = $this->getUser();
         $out = false;
-        foreach($roles as $role) {
+        foreach ($roles as $role) {
             $out != $user->hasRole($role);
         }
         return $out;
     }
-    
+
+    /**
+     * Fa il SELECT * da un repository o esegue una query
+     * 
+     * @param string $classe nome del repository
+     * @param array $find array con i criteri di ricerca
+     * @return Mixed
+     */
+    protected function countDql($classe, $find = array()) {
+        $repo = $this->getRepository($classe);
+        /* @var $repo \Doctrine\ORM\EntityRepository */
+        $qb = $repo->createQueryBuilder('c')->select('COUNT(c)');
+        foreach ($find as $key => $value) {
+            $qb->andWhere("c.{$key} = :{$key}")
+                    ->setParameter($key, $value);
+        }
+        $out = $qb->getQuery()->getScalarResult();
+        return intval($out[0][1]);
+    }
+
+    /**
+     * Fa il SELECT COUNT(*) usando una query SQL
+     * C'è il controllo che sia SELECT COUNT(*) FROM o scoppia
+     * 
+     * @param string $sql query sql
+     * @param array $params array con i criteri di ricerca
+     * @return Mixed
+     */
+    protected function countSql($sql, $params = array()) {
+        if (!preg_match('/^[ \n\r\t]*select[ \n\r\t]+count\(\*\)[ \n\r\t]+from[ \n\r\t]+/i', $sql)) {
+            $sql = 'SELECT COUNT(*) FROM ' . $sql;
+        }
+        $em = $this->getEm();
+        /* @var $em \Doctrine\ORM\EntityManager */
+        $conn = $em->getConnection();
+        $stmt = $conn->executeQuery($sql, $params);
+        $out = $stmt->fetch();
+        return intval(array_shift($out));
+    }
+
 }
