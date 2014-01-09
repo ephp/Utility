b30c28c3f4031896570f489c875fb19082857e54
Ephraim <ephraim.pepe@gmail.com>
2014-01-09 17:04:11 +0100

    Fix fx autosave e fx i18n

--- Resources/public/js/functions.js
+++ Resources/public/js/functions.js
@@ -8,8 +8,8 @@ function crossfade(classe, time, fade, index) {
     if (!index) {
         index = 1;
     }
-    var min = 1; 
-    var max = $('.' + classe).length; 
+    var min = 1;
+    var max = $('.' + classe).length;
     if (index > max) {
         index = max;
     }
@@ -17,7 +17,7 @@ function crossfade(classe, time, fade, index) {
     $('#' + classe + '-' + index).show();
     setInterval(function() {
         var next = index + 1;
-        if(next > max) {
+        if (next > max) {
             next = 1;
         }
         $('#' + classe + '-' + index).slideToggle(fade);
@@ -27,37 +27,74 @@ function crossfade(classe, time, fade, index) {
 }
 
 function autoupdateDiv(classe) {
-    $('.' + classe).blur(function(){
-        var r = null;
-        var rp = {};
-        var u = null;
-        var p = {};
-        if($(this).attr('route')) {
-            r = $(this).attr('route');
-        }
-        if($(this).attr('route-param')) {
-            rp = JSON.parse($(this).attr('route-param'));
-        }
-        if($(this).attr('url')) {
-            u = $(this).attr('url');
-        }
-        if($(this).attr('param')) {
-            p = JSON.parse($(this).attr('param'));
-        }
-        var v = $(this).html();
-        var f = $(this).attr('name');
-        p = Object.merge({v: v, f: f}, p);
-        if(r) {
-            $.post(Routing.generate(r, rp), p, function(data) {
-                console.log(data);
+    var old = null;
+    var btn = false;
+    $('.' + classe)
+            .focus(function() {
+                old = $(this).html();
+            })
+            .keyup(function() {
+                if (old === $(this).html()) {
+                    if (btn) {
+                        $('#autoupdate-btn').remove();
+                        btn = false;
+                    }
+                } else {
+                    if (!btn) {
+                        $(this).after('<button type="button" id="autoupdate-btn" class="btn">'+i18n('Save')+'</button>');
+                        btn = true;
+                    }
+                }
+            })
+            .blur(function() {
+                if (btn) {
+                    var $this = $(this);
+                    var r = null;
+                    var rp = {};
+                    var u = null;
+                    var p = {};
+                    if ($this.attr('route')) {
+                        r = $this.attr('route');
+                    }
+                    if ($this.attr('route-param')) {
+                        rp = JSON.parse($this.attr('route-param'));
+                    }
+                    if ($this.attr('url')) {
+                        u = $this.attr('url');
+                    }
+                    if ($this.attr('param')) {
+                        p = JSON.parse($this.attr('param'));
+                    }
+                    var v = $this.html();
+                    var f = $this.attr('name');
+                    p = Object.merge({v: v, f: f}, p);
+                    if (r) {
+                        u = Routing.generate(r, rp);
+                    }
+                    if (u) {
+                        $('#autoupdate-btn').text(i18n('Saving'));
+                        $.post(u, p, function(data) {
+                            $('#autoupdate-btn').text(i18n('Saved'));
+                            $('#autoupdate-btn').fadeOut(function() {
+                                $('#autoupdate-btn').remove();
+                            });
+                        });
+                    } else {
+                        alert('Specificare il parametro "route" o il parametro "url"')
+                    }
+                }
             });
-        } else if(u) {
-            $.post(u, p, function(data) {
-                console.log(data);
-            });
+
+}
+
+function i18n(frase, dominio) {
+    if(window.Translator) {
+        if(dominio) {
+            return Translator.trans(frase, {}, dominio);
         } else {
-            alert('Specificare il parametro "route" o il parametro "url"')
+            return Translator.trans(frase);
         }
-    });
-    
+    } else {
+        return frase;
+    }
 }
