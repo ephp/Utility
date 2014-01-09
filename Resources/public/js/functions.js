04845efd96c5b99f5abe875d4d20b359a4056274
Ephraim <ephraim.pepe@gmail.com>
2014-01-09 16:16:18 +0100

    add fx

--- Resources/public/js/functions.js
+++ Resources/public/js/functions.js
@@ -25,3 +25,39 @@ function crossfade(classe, time, fade, index) {
         index = next;
     }, time);
 }
+
+function autoupdateDiv(classe) {
+    $('.' + classe).blur(function(){
+        var r = null;
+        var rp = {};
+        var u = null;
+        var p = {};
+        if($(this).attr('route')) {
+            r = $(this).attr('route');
+        }
+        if($(this).attr('route-param')) {
+            rp = JSON.parse($(this).attr('route-param'));
+        }
+        if($(this).attr('url')) {
+            u = $(this).attr('url');
+        }
+        if($(this).attr('param')) {
+            p = JSON.parse($(this).attr('param'));
+        }
+        var v = $(this).html();
+        var f = $(this).attr('name');
+        p = Object.merge({v: v, f: f}, p);
+        if(r) {
+            $.post(Routing.generate(r, rp), p, function(data) {
+                console.log(data);
+            });
+        } else if(u) {
+            $.post(u, p, function(data) {
+                console.log(data);
+            });
+        } else {
+            alert('Specificare il parametro "route" o il parametro "url"')
+        }
+    });
+    
+}
